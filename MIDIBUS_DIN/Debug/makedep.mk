8dd4c62dba71887fcb0059e09e4572962a013633
GuavTek <36814224+GuavTek@users.noreply.github.com>
2021-10-18 17:47:22 +0200

    Add ASF

--- MIDIBUS_DIN/Debug/makedep.mk
+++ MIDIBUS_DIN/Debug/makedep.mk
@@ -2,9 +2,33 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-Device_Startup\startup_samd21.c
-
-Device_Startup\system_samd21.c
-
 main.cpp
+
+MCP2517_ATSAMD_driver\MCP2517.cpp
+
+MCP2517_ATSAMD_driver\SPI.cpp
+
+MIDI_driver\MIDI_Driver.cpp
+
+src\ASF\common2\boards\user_board\init.c
+
+src\ASF\common\utils\interrupt\interrupt_sam_nvic.c
+
+src\ASF\sam0\drivers\port\port.c
+
+src\ASF\sam0\drivers\system\clock\clock_samd21_r21_da_ha1\clock.c
+
+src\ASF\sam0\drivers\system\clock\clock_samd21_r21_da_ha1\gclk.c
+
+src\ASF\sam0\drivers\system\interrupt\system_interrupt.c
+
+src\ASF\sam0\drivers\system\pinmux\pinmux.c
+
+src\ASF\sam0\drivers\system\system.c
+
+src\ASF\sam0\utils\cmsis\samd21\source\gcc\startup_samd21.c
+
+src\ASF\sam0\utils\cmsis\samd21\source\system_samd21.c
+
+src\ASF\sam0\utils\syscalls\gcc\syscalls.c
 
